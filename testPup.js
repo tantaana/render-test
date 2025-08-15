4d7c76a154a206ea39de19aba509c34457f34d80
MD. Shariar Tanvir <108363843+tantaana@users.noreply.github.com>
2025-08-15 17:41:00 +0600

    Update testPup.js

--- testPup.js
+++ testPup.js
@@ -3,7 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath: '/usr/bin/google-chrome' // Add this line
     });
     const page = await browser.newPage();
     await page.goto('https://example.com');
